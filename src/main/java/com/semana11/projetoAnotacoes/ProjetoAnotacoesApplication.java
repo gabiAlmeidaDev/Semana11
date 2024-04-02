9380b35254d3540427ebc274795fac05935f8059
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-04-01 21:30:06 -0300

    first commit

---
+++ src/main/java/com/semana11/projetoAnotacoes/ProjetoAnotacoesApplication.java
@@ -0,0 +1,13 @@
+package com.semana11.projetoAnotacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAnotacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAnotacoesApplication.class, args);
+	}
+
+}
